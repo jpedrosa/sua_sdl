947548d97c08be07f04539e38ed46e2b8c23ffc3
Joao Pedrosa <joaopedrosa@gmail.com>
2016-02-02 15:40:09 -0300

    Start incorporating Hexacoral.

---
+++ Sources/SuaSDL/hexacoral.swift
@@ -0,0 +1,216 @@
+
+import _Sua
+
+
+public struct RGBAColor {
+  var r: UInt8
+  var g: UInt8
+  var b: UInt8
+  var a: UInt8?
+}
+
+
+public struct Hexacoral {
+  public var style = 0
+  public var color: RGBAColor?
+  public var backgroundColor: RGBAColor?
+
+  public static let BOLD          = 2
+  public static let UNDERLINE     = 4
+  public static let ITALIC        = 8
+  public static let STRIKEOUT     = 16
+
+  public static func matchHexa(c: UInt8) -> Bool {
+    return (c >= 97 && c <= 102) || (c >= 65 && c <= 70) ||
+        (c >= 48 && c <= 57)
+  }
+
+  // The sequence has to end in either = or , (comma).
+  public static func parseHexaSequence(bytes: [UInt8], startIndex: Int,
+      maxBytes: Int) throws -> (RGBAColor?, Int) {
+    var i = startIndex
+    let hti = HexaUtils.hexaToInt
+    let c1 = bytes[i]
+    if matchHexa(c1) && i + 3 < maxBytes {
+      let c2 = bytes[i + 1]
+      let c3 = bytes[i + 2]
+      if matchHexa(c2) && matchHexa(c3) {
+        i += 3
+        let c4 = bytes[i]
+        if c4 == 44 || c4 == 61 { // , =
+          return (RGBAColor(r: hti(c1, c2: c1)!,
+                           g: hti(c2, c2: c2)!,
+                           b: hti(c3, c2: c3)!,
+                           a: nil), i)
+        } else if matchHexa(c4) {
+          i += 1
+          if i < maxBytes {
+            let c5 = bytes[i]
+            if c5 == 44 || c5 == 61 { // , =
+              return (RGBAColor(r: hti(c1, c2: c1)!,
+                               g: hti(c2, c2: c2)!,
+                               b: hti(c3, c2: c3)!,
+                               a: hti(c4, c2: c4)), i)
+            } else if matchHexa(c5) && i + 2 < maxBytes {
+              i += 1
+              let c6 = bytes[i]
+              if matchHexa(c6) {
+                i += 1
+                let c7 = bytes[i]
+                if c7 == 44 || c7 == 61 { // , =
+                  return (RGBAColor(r: hti(c1, c2: c2)!,
+                                   g: hti(c3, c2: c4)!,
+                                   b: hti(c5, c2: c6)!,
+                                   a: nil), i)
+                } else if matchHexa(c7) && i + 2 < maxBytes {
+                  i += 1
+                  let c8 = bytes[i]
+                  if matchHexa(c8) {
+                    i += 1
+                    let c9 = bytes[i]
+                    if c9 == 44 || c9 == 61 { // , =
+                      return (RGBAColor(r: hti(c1, c2: c2)!,
+                                       g: hti(c3, c2: c4)!,
+                                       b: hti(c5, c2: c6)!,
+                                       a: hti(c7, c2: c8)), i)
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return (nil, i)
+  }
+
+  // Starts from %^b#=. It starts past the percent character.
+  public static func parseHexacoral(bytes: [UInt8], startIndex: Int,
+      maxBytes: Int) throws -> (Hexacoral?, Int) {
+    var i = startIndex
+    var style = 0
+    func parseColor() throws -> Hexacoral? {
+      let (maybeColor, advi) = try parseHexaSequence(bytes, startIndex: i,
+          maxBytes: maxBytes)
+      i = advi
+      if let color = maybeColor {
+        let c = bytes[i]
+        if c == 44 { // ,
+          i += 1
+          if i < maxBytes {
+            let (maybeBgColor, advi) = try parseHexaSequence(bytes,
+                startIndex: i, maxBytes: maxBytes)
+            i = advi
+            if let bgColor = maybeBgColor {
+              if bytes[i] == 61 { // =
+                return Hexacoral(style: style, color: color,
+                    backgroundColor: bgColor)
+              } else { // ,
+                // Ignore.
+              }
+            } else {
+              i -= 1 // Go back once, because the main while loop already
+                     // increments it.
+            }
+          }
+        } else if c == 61 { // =
+          return Hexacoral(style: style, color: color, backgroundColor: nil)
+        } else {
+          throw HexacoralError.Unreachable
+        }
+      }
+      return nil
+    }
+    if bytes[i] == 35 { // #
+      i += 1
+      if bytes[i] == 61 { // =
+        return (Hexacoral(style:0, color: nil, backgroundColor: nil), i)
+      } else {
+        return (try parseColor(), i)
+      }
+    } else {
+      STYLE: repeat {
+        switch bytes[i] {
+          case 98: // b
+            if style & Hexacoral.BOLD > 0 {
+              break STYLE
+            }
+            style |= Hexacoral.BOLD
+          case 117: // u
+            if style & Hexacoral.UNDERLINE > 0 {
+              break STYLE
+            }
+            style |= Hexacoral.UNDERLINE
+          case 115: // s
+            if style & Hexacoral.STRIKEOUT > 0 {
+              break STYLE
+            }
+            style |= Hexacoral.STRIKEOUT
+          case 105: // i
+            if style & Hexacoral.ITALIC > 0 {
+              break STYLE
+            }
+            style |= Hexacoral.ITALIC
+          case 35: // #
+            i += 1
+            if i < maxBytes {
+              if bytes[i] == 61 { // =
+                return (Hexacoral(style: style, color: nil,
+                    backgroundColor: nil), i)
+              } else {
+                return (try parseColor(), i)
+              }
+            }
+            break STYLE
+          default: break STYLE
+        }
+        i += 1
+      } while i < maxBytes
+    }
+    return (nil, i)
+  }
+
+  public static func parseText(bytes: [UInt8]) throws
+      -> [(String, Hexacoral?)] {
+    var tokenIndex = 0
+    let len = bytes.count
+    var list = [(String, Hexacoral?)]()
+    var i = 0
+    let vlen = len - 2
+    var hexac: Hexacoral?
+    func collect(ei: Int) throws {
+      if let z = String.fromCharCodes(bytes, start: tokenIndex, end: ei) {
+        list.append((z, hexac))
+      } else {
+        throw HexacoralError.Unicode
+      }
+    }
+    while i < vlen {
+      if bytes[i] == 37 { // %
+        let (hc, advi) = try parseHexacoral(bytes, startIndex: i + 1,
+            maxBytes: len)
+        if let _ = hc {
+          if i - 1 >= tokenIndex {
+            try collect(i - 1)
+          }
+          tokenIndex = advi + 1
+        }
+        hexac = hc
+        i = advi
+      }
+      i += 1
+    }
+    if tokenIndex < len {
+      try collect(len)
+    }
+    return list
+  }
+
+}
+
+public enum HexacoralError: ErrorType {
+  case Parse
+  case Unicode
+  case Unreachable
+}
---
+++ docs/Hexacoral.md
@@ -0,0 +1,108 @@
+Hexacoral
+---------
+
+Hexacoral is a new encoding format for both style and color that can be embedded
+in ordinary text.
+
+The chosen format is this one:
+
+    %busi#FFFFFFFF,FFFFFFFF=            // Extended, most detailed one.
+    %busi#FFFF,FFFF=                    // Detailed by including the alpha code.
+    %busi#FFF,FFF=                      // Now without the alpha code.
+    %busi#FFF=                          // No background this time.
+    %bus#FFF=                           // No italics style for this one.
+    %#FFF=                              // Skip style entirely.
+    %#=                                 // Not even foreground color now. Reset.
+
+Where the styles can be enabled by being included between % and # characters.
+
+  * b - Bold.
+
+  * u - Underline.
+
+  * s - Strikeout or strikethrough.
+
+  * i - Italic.
+
+The rule is that their letter needs to be in lower case and can only be included
+once. More than once and the entire code will be ignored and shown in plain
+text.
+
+The rule also says that if the code cannot be understood, the entire code will
+be shown in plain text. This way it could help with debugging it or even in case
+the code was not actually an Hexacoral code to begin with, but just happened to
+have about the same characters.
+
+Following the hash character (#) we have the hexadecimal colors, which are based
+on the formats found on the web.
+
+  * The hexadecimal color can have just 3 characters which is enough to encode
+12 bit of color. It is a shorthand format that helps with saving bytes and
+typing.
+
+E.g.
+
+    %#FFF=               // White. Same as %#FFFFFF=
+    %#F00=               // Red. Same as %#FF0000=
+    %#913=               // Mixed. Same as %#991133=
+
+So that each character gets doubled in its full value representation.
+
+  * The hexadecimal color can include the alpha channel code -- the 4th
+character in the shorthand format. Or even the fourth character set on the full
+format representation. While the alpha channel may be ignored when it's
+interpreted by some program, it's good to be able to encode it as well. Which I
+think is not possible on the web hexadecimal format by default. On the web I
+have seen the longer formatting with rgba(0, 0, 0, 0) instead for the alpha
+channel.
+
+E.g.
+
+    %#00F7=               // Blue with some transparency. Same as %#0000FF77=
+
+  * The background color can also be set with the comma following the foreground
+color. So to be able to set the background color, the foreground color needs to
+be set first. This could help with making sure that the colors have a good
+contrast between themselves.
+
+E.g.
+
+    %#FFF,00F=            // White text on blue background.
+                          // Same as %#FFFFFF,0000FF=
+
+  * Finally, the empty hexacoral code helps to reset it by removing all of the
+styles and colors set beforehand.
+
+E.g.
+
+    %#=                   // This resets the styles and colors; back to default.
+
+Hexacoral codes are only valid up to the next hexacoral code. So in effect the
+next hexacoral code resets the one before it. It also gives more of a WYSIWYG
+feel to it as you can be sure that the latest hexacoral code is the only set of
+styles and colors that are present for the text following it. While this can
+make it a bit more work when you do want a previous code to carry through other
+codes, given the conciseness with which the hexacoral code can encode a
+multitude of styles and colors, you could just repeat the setting for the next
+one until you were done with it.
+
+-----------------
+
+The idea for this encoding format started as I wanted to be able to embed
+literal text that included styles and colors into the SuaSDL project. I wanted
+it to be concise, but that it was not HTML or some other format that went beyond
+just the styles and colors.
+
+I have seen colors being embedded in text starting with the mIRC Script and even
+before that in BBS systems. Terminals can also include ANSI color codes. But
+nowadays we have 32 bit colors and lots of pixels to fill them with. Being able
+to encode all of those colors was one of the goals of this project.
+
+Another goal was that the codes be plain text so that they can be stored,
+edited, viewed and sent over the network to other clients. By being plain text
+there is always the chance that the format could naturally occur in some text
+somewhere and that could cause some confusion -- it should be a small chance
+though.
+
+These hexacoral codes could be used in some chatting program somewhere, say on
+Twitch bots or some such.
